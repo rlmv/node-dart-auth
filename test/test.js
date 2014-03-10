21198b370f62406882fad2763afbb1ffd3ca112d
Robert Marchman <robert.l.marchman@dartmouth.edu>
2014-03-10 00:38:17 -0400

    basic redirect test

--- test/test.js
+++ test/test.js
@@ -2,8 +2,8 @@
 var request = require('supertest');
 var express = require('express');
 var assert = require('assert');
+var Browser = require('zombie');
 var auth = require('..'); // module
-
 
 var app = express();
 
@@ -12,8 +12,17 @@ app.use(express.session());
 
 app.use(auth({ service: 't' }));
 app.get('/', function(req, res) {
-    res.send(200, {name: 'tobi'});
+    res.send(200, {name: 'test'});
 });
+
+
+describe('Environment', function() {
+    it('USR and PWD environment vars must exist', function() {
+        assert.ok(process.env.PWD);
+        assert.ok(process.env.USR);
+    });
+});
+
 
 describe('Initialization', function() {
     describe('.auth()', function() {
@@ -23,24 +32,38 @@ describe('Initialization', function() {
     });
 
     describe('.auth({options})', function() {
-        it('should initialize', function() {
+        it('should initialize without error', function() {
             auth({ service: 'localhost:3000' });
         });
     });
 });
 
 describe('Should redirect GET /', function() {
- it('respond', function(done) {
-    request(app)
-    .get('/')
-    .expect(307)
-    .end(function(err, res) {
-        if (err) throw err;
-        var location = res.get('location'); // pull redirect url
+    this.timeout(10000); // need extra time for redirects
 
-        done();
+    it('redirect and authenticate', function(done) {
+        request(app)
+        .get('/')
+        .expect(307)
+        .end(function(err, res) {
+  
+            if (err) throw err;
+            var location = res.get('location'); // pull redirect url
+
+            browser = new Browser();
+            browser.visit(location)
+            .then(function() {
+                browser.fill('username', process.env.USR);
+                browser.fill('password', process.env.PWD);
+                return browser.pressButton('sSubmit');
+            })
+            .then(function() {
+                // should redirect to localhost app
+                assert.equal(browser.location.hostname, '127.0.0.1');
+            })
+            .then(done, done);
+        });
     });
- });
 });
 
 
